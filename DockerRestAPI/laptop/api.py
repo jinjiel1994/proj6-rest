1429c485280404b2ff14803224c50efa2ff2e6e9
Jim Li <jinjiel1994@gmail.com>
2018-03-02 22:16:10 -0800

    finished

--- DockerRestAPI/laptop/api.py
+++ DockerRestAPI/laptop/api.py
@@ -1,25 +1,144 @@
 # Laptop Service
-
+import flask
+import pymongo
 from flask import Flask
 from flask_restful import Resource, Api
 
 # Instantiate the app
+from pymongo import MongoClient
+
 app = Flask(__name__)
 api = Api(app)
 
-class Laptop(Resource):
+
+client = MongoClient('db', 27017)
+db = client.time
+
+
+class listAll(Resource):
     def get(self):
-        return {
-            'Laptops': ['Mac OS', 'Dell', 
-            'Windozzee',
-	    'Yet another laptop!',
-	    'Yet yet another laptop!'
-            ]
-        }
+        _items = db.time.find()
+        items = [item for item in _items]
+
+        return {'km': [item['km'] for item in items],
+                'open': [item["open"] for item in items],
+                'close': [item["close"] for item in items]
+                }
+
+
+class listOpenOnly(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+
+        return {'open': [item["open"] for item in items]
+                }
+
+
+class listCloseOnly(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+
+        return {'close': [item["close"] for item in items]
+                }
+
+
+class listAllcsv(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+        csv = 'open, close\n'
+        for item in items:
+            csv += '%s, %s\n' % (item['open'], item['close'])
+        csv = csv.strip('\n')
+        csv = csv.split('\n')
+        return csv
+
+
+class listOpenOnlycsv(Resource):
+    def get(self):
+
+        top = flask.request.args.get("top", type=int)
+        if top is None:
+            _items = db.time.find()
+        else:
+            _items = db.time.find().sort("open", pymongo.ASCENDING).limit(top)
+
+        items = [item for item in _items]
+        csv = 'open\n'
+        for item in items:
+            csv += '%s\n' % item['open']
+        csv = csv.strip('\n')
+        csv = csv.split('\n')
+        return csv
+
+
+class listCloseOnlycsv(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+        csv = 'close\n'
+        for item in items:
+            csv += '%s\n' % item['close']
+        csv = csv.strip('\n')
+        csv = csv.split('\n')
+        return csv
+
+
+class listAlljson(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+        json = []
+        for item in items:
+            json.append({'km': item['km'],
+                'open': item['open'],
+                'close': item['close']
+                })
+        return json
+
+
+class listOpenOnlyjson(Resource):
+    def get(self):
+        top = flask.request.args.get("top", type=int)
+        if top is None:
+            _items = db.time.find()
+        else:
+            _items = db.time.find().sort("open", pymongo.ASCENDING).limit(top)
+
+        items = [item for item in _items]
+        json = []
+        for item in items:
+            json.append({'km': item['km'],
+                         'open': item['open']
+                        })
+        return json
+
+
+class listCloseOnlyjson(Resource):
+    def get(self):
+        _items = db.time.find()
+        items = [item for item in _items]
+        json = []
+        for item in items:
+            json.append({'km': item['km'],
+                         'close': item['close']
+                        })
+        return json
+
 
 # Create routes
 # Another way, without decorators
-api.add_resource(Laptop, '/')
+api.add_resource(listAll, '/listAll')
+api.add_resource(listOpenOnly, '/listOpenOnly')
+api.add_resource(listCloseOnly, '/listCloseOnly')
+api.add_resource(listAllcsv, '/listAll/csv')
+api.add_resource(listOpenOnlycsv, '/listOpenOnly/csv')
+api.add_resource(listCloseOnlycsv, '/listCloseOnly/csv')
+api.add_resource(listAlljson, '/listAll/json')
+api.add_resource(listOpenOnlyjson, '/listOpenOnly/json')
+api.add_resource(listCloseOnlyjson, '/listCloseOnly/json')
 
 # Run the application
 if __name__ == '__main__':
--- DockerRestAPI/website/index.php
+++ DockerRestAPI/website/index.php
@@ -1,19 +1,101 @@
 <html>
     <head>
-        <title>CIS 322 REST-api demo: Laptop list</title>
+        <title>Homepage</title>
     </head>
+    <style>
+        table, th, td {
+        border: 1px solid black;
+    }
+    </style>
 
     <body>
-        <h1>List of laptops</h1>
+        <h1>ListAll<a/></h1>
         <ul>
             <?php
-            $json = file_get_contents('http://laptop-service/');
+            $json = file_get_contents('http://laptop-service/listAll');
             $obj = json_decode($json);
-	          $laptops = $obj->Laptops;
-            foreach ($laptops as $l) {
+	        $open = $obj->open;
+	        $close = $obj->close;
+	        $km = $obj->km;
+	        echo "<table><tr><th>open</th><th>close</th><th>km</th></tr>";
+            foreach ($open as $key=>$value) {
+                echo "<tr><td>$open[$key]</td><td>$close[$key]</td><td>$km[$key]</td></tr>";
+            }
+            echo "</table>";
+            ?>
+        </ul>
+        <h1>listOpenOnly<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnly');
+            $obj = json_decode($json);
+	        $open = $obj->open;
+	        echo "<table><tr><th>open</th></tr>";
+            foreach ($open as $key=>$value) {
+                echo "<tr><td>$open[$key]</td></tr>";
+            }
+            echo "</table>";
+            ?>
+        </ul>
+        <h1>listCloseOnly<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listCloseOnly');
+            $obj = json_decode($json);
+	        $close = $obj->close;
+	        echo "<table><tr><th>close</th></tr>";
+            foreach ($close as $key=>$value) {
+                echo "<tr><td>$close[$key]</td></tr>";
+            }
+            echo "</table>";
+            ?>
+        </ul>
+        <h1>listAllcsv<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listAll/csv');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {
                 echo "<li>$l</li>";
             }
             ?>
         </ul>
+        <h1>listOpenOnlycsv<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnly/csv');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {
+                echo "<li>$l</li>";
+            }
+            ?>
+        </ul>
+        <h1>listOpenOnlycsv with top = 3<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnly/csv?top=3');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {
+                echo "<li>$l</li>";
+            }
+            ?>
+        </ul>
+        <h1>listCloseOnlycsv<a/></h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listCloseOnly/csv');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {
+                echo "<li>$l</li>";
+            }
+            ?>
+        </ul>
+        <h1><a href="http://0.0.0.0:5001/listAll/json">listAlljson</a></h1>
+        <h1><a href="http://0.0.0.0:5001/listOpenOnly/json">listOpenOnlyjson</a></h1>
+        <h1><a href="http://0.0.0.0:5001/listOpenOnly/json?top=3">listOpenOnlyjson with top = 3</a></h1>
+        <h1><a href="http://0.0.0.0:5001/listCloseOnly/json">listCloseOnlyjson</a></h1>
+
+
     </body>
 </html>
+
